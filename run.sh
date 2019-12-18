2cb5b140ff81adad10392de50d77940260f6ba97
Cory Lueninghoener <cluening@lanl.gov>
2019-12-17 19:29:48 -0700

    Moves web files around; adds the postgres fuzzy string matching extension

--- run.sh
+++ run.sh
@@ -31,7 +31,8 @@ if [ "$1" = "import" ]; then
     sudo -u postgres createdb -E UTF8 -O renderer gis
     sudo -u postgres psql -d gis -c "CREATE EXTENSION postgis;"
     sudo -u postgres psql -d gis -c "CREATE EXTENSION hstore;"
-    sudo -u postgres psql -d gis -c "CREATE EXTENSION fuzzystrmatch;"
+    #sudo -u postgres psql -d gis -c "CREATE EXTENSION fuzzystrmatch;"
+    sudo -u postgres psql -d gis -c "CREATE EXTENSION pg_trgm;"
     sudo -u postgres psql -d gis -c "ALTER TABLE geometry_columns OWNER TO renderer;"
     sudo -u postgres psql -d gis -c "ALTER TABLE spatial_ref_sys OWNER TO renderer;"
     setPostgresPassword
