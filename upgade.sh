9f7d3f8495869c95c0b83b6d6f008882f956eae3
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2021-06-12 09:25:03 +0200

    Update upgade.sh

--- upgade.sh
+++ upgade.sh
@@ -6,14 +6,14 @@ docker run --rm  \
   -v /${CWD}://tmp  \
   -v openstreetmap-data://var/lib/postgresql/12/main  \
   -w //tmp  \
-  postgres:12  \
+  postgis/postgis:12-3.1 \
   pg_dumpall -U prostgres > dump.sql
 
 docker run --rm  \
   -v /${CWD}://tmp  \
   -v openstreetmap-data://var/lib/postgresql/13/main  \
   -w //tmp  \
-  postgres:13  \
+  postgis/postgis:13-3.1  \
   psql -U postgres -d gis < dump.sql
 
 rm ./dump.sql
