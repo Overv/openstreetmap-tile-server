5572c9722ef7cc478d0143436b9f723c54171527
Robin C. Ladiges <Istador@users.noreply.github.com>
2022-05-02 00:43:17 +0200

    create /data/database/postgres/ if missing (#273)
    fix for issue #271

--- run.sh
+++ run.sh
@@ -42,6 +42,8 @@ fi
 
 if [ "$1" == "import" ]; then
     # Ensure that database directory is in right state
+    mkdir -p /data/database/postgres/
+    chown renderer: /data/database/
     chown -R postgres: /var/lib/postgresql /data/database/postgres/
     if [ ! -f /data/database/postgres/PG_VERSION ]; then
         sudo -u postgres /usr/lib/postgresql/14/bin/pg_ctl -D /data/database/postgres/ initdb -o "--locale C.UTF-8"
